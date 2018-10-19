d734e94c154445290986bbd67ebf1d076c60c148
GioeleSLFierro <gioele5@gmail.com>
2018-10-19 11:20:34 +0200

    Populate view

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
-from MainLoop import MainLoop
-from Settings import Settings
+from app.MainLoop import MainLoop
+from app.Settings import Settings
 
+# Load flask app
 app = Flask(__name__)
+
+# Load project objects
 loop = MainLoop()
 settings = Settings()
+
+# Load prject properties
+suppl_heating = False
+suppl_heating_status = False
+
+
 
 from app import routes
--- app/routes.py
+++ app/routes.py
@@ -1,26 +1,32 @@
 from flask import render_template, request
-from raspberryManagemant import getTemp
-from app import app
+from app.raspberryManagemant import getTemp
+from app import app, settings, suppl_heating, suppl_heating_status
 
 @app.route('/',methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 def index():
    
-    # Read current temperature
+    # Read settings/data and populate view
     current_temp = getTemp()
+    thresholdMIN = settings.thresholdMIN
+    thresholdMAX = settings.thresholdMAX
 
+    shs = 'on' if suppl_heating_status else 'off'
+    sh = 'on' if suppl_heating else 'off'
+
+    # From managemant
     form_data = request.form 
     if(form_data):
-        
-        # Read form data
-        #TODO form_data['threshold_min']
-
         # Save form data
-        #TODO
+        settings.thresholdMIN = form_data['threshold_min']
+        settings.thresholdMAX = form_data['threshold_max']
     else:
         # Read saved settings
         #TODO
+        pass
 
     # Run main loop
+    #TODO
 
-    return render_template('index.html', title='Home')
+    # Load template
+    return render_template('index.html', temp = current_temp, shs = shs, heating_threshold_MAX = thresholdMAX, heating_threshold_MIN = thresholdMIN, sh = sh)
